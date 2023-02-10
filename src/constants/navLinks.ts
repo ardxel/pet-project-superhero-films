419d63273c39299938b9f0724f4021954fea201e
ardxel <ardxel@gmail.com>
2023-02-10 03:28:22 +0300

    new constants

---
+++ src/constants/navLinks.ts
@@ -0,0 +1,13 @@
+const navLinks: Array<{
+  readonly id: number;
+  readonly name: string;
+  readonly path: string;
+}> = [
+  { id: 0, name: 'Home', path: '' },
+  { id: 1, name: 'New', path: '' },
+  { id: 2, name: 'Marvel', path: '' },
+  { id: 3, name: 'DC', path: '' },
+  { id: 4, name: 'Sign in', path: '' },
+];
+
+export default navLinks;
