dea49391974a0c3b2cd8026085a977ba997ca89d
ardxel <ardxel@gmail.com>
2023-02-11 10:06:38 +0300

    custom hooks

---
+++ src/hooks/useTheme.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import { getTheme } from '../tools/helpers/getTheme';
+
+const useTheme = (): { theme: string; changeTheme: () => void } => {
+  const [theme, setTheme] = useState(
+    getTheme({ light: 'light', dark: 'dark' })
+  );
+
+  const changeTheme = () => {
+    if (theme === 'light') setTheme('dark');
+    else setTheme('light');
+  };
+
+  useEffect(() => {
+    document.documentElement.dataset.theme = theme;
+    localStorage.setItem('theme', JSON.stringify(theme));
+  }, [theme]);
+
+  return { theme, changeTheme };
+};
+
+export default useTheme;
