0e304ce6afb297f8cce82c644599aa2ee9d15d6f
ardxel <ardxel@gmail.com>
2023-02-15 12:02:17 +0300

    change devtool

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,12 +1,12 @@
-const { merge } = require('webpack-merge')
-const common = require('./webpack.common')
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
 
 module.exports = function (env, argv) {
-	const configFromCommon = common(env, argv)
+  const configFromCommon = common(env, argv);
 
-	const result = {
-		devtool: 'source-map',
-	}
+  const result = {
+    devtool: 'cheap-module-source-map',
+  };
 
-	return merge([configFromCommon, result])
-}
+  return merge([configFromCommon, result]);
+};
