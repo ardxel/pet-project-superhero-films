48337c42bfd8d4453487b262fa03cb08fc58f750
ardxel <ardxel@gmail.com>
2023-02-15 12:07:04 +0300

    new methods for server

--- config/webpack.mock.ts
+++ config/webpack.mock.ts
@@ -1,10 +1,15 @@
 import webpackMockServer from 'webpack-mock-server';
-import fs from 'fs';
-import nodePath from 'path';
 import cors from 'cors';
+import Data from './tools/mockHelper';
+import IMovie from 'types/Movie';
 
 export default webpackMockServer.add((app, helper) => {
   app.use(cors());
+  app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    next();
+  })
   app.get('/testGet', (req, res) => {
     res.json('JS get-object can be here. Random int:' + helper.getRandomInt());
   });
@@ -12,29 +17,22 @@ export default webpackMockServer.add((app, helper) => {
     res.json('JS post-object can be here');
   });
 
-  // let movies = JSON.parse(
-  //   fs.readFileSync(nodePath.join(__dirname, '../data/movies.json'), 'utf-8')
-  // );
-  // let users = JSON.parse(
-  //   fs.readFileSync(nodePath.join(__dirname, '../data/users.json'), 'utf-8')
-  // );
-  // console.log(users);
   console.log(
-    JSON.parse(fs.readFileSync(nodePath.join(__dirname, '../data/movies.json'),'utf-8'))
-      .map(item => {
-        return item.nameRu
-      })
-  )
+    'MARVEL: ',
+    Data.parseData<IMovie>('marvel').map((item) => item.nameRu)
+  );
+  console.log(
+    'DC: ',
+    Data.parseData<IMovie>('dc').map((item) => item.nameRu)
+  );
 
+  // localhost:3000/root
   app.post('/save-movie', (req, res) => {
     let movie = req.body;
     console.log(movie);
+    const filename = movie.comic === 'dc' ? 'dc' : 'marvel';
 
-    const data = fs.readFileSync(
-      nodePath.join(__dirname, '../data/movies.json'),
-      'utf-8'
-    );
-    const movies = JSON.parse(data);
+    const movies = Data.parseData(filename) as any[];
 
     const isRepeatMovie = movies.find(
       (item) => item.kinopoiskId === movie.kinopoiskId
@@ -45,13 +43,23 @@ export default webpackMockServer.add((app, helper) => {
       console.log('this film has already been added');
       return;
     }
-    movie = {id: movies.length+1, ...movie};
-
+    movie = { id: movies.length + 1, ...movie };
     movies.push(movie);
-
-    const jsonData = JSON.stringify(movies);
-    fs.writeFileSync(nodePath.join(__dirname, '../data/movies.json'), jsonData);
-
+    Data.writeData(movies, filename);
     res.sendStatus(200);
   });
-});
+
+  app.get('/movies-all', (req, res) => {
+    console.log(Data.listOfDataFiles)
+    const movies = Data.parseAllMoviesData();
+    res.send(movies);
+  });
+  app.get('/franchise=:name', (req, res) => {
+    const name = req.params.name;
+    const movies = Data
+      .parseAllMoviesData()
+      .filter(movie => movie.nameOriginal.includes(name))
+      .sort((a,b) => b.year - a.year);
+    res.send(movies);
+  })
+})
