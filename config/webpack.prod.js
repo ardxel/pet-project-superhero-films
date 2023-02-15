23e83c13b2326fce075bbea5f1b5371d15c34137
ardxel <ardxel@gmail.com>
2023-02-15 12:08:35 +0300

    new plugin and some feauter

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,18 +1,35 @@
-const { merge } = require('webpack-merge')
-const common = require('./webpack.common')
-const TerserPlugin = require('terser-webpack-plugin')
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = function (env, argv) {
-	const configFromCommon = common(env, argv)
-	const result = {
-		mode: 'production',
-		output: {
-			filename: '[name].[contenthash].js',
-		},
-		optimization: {
-			minimizer: [new TerserPlugin({})],
-		},
-	}
+  const configFromCommon = common(env, argv);
+  const result = {
+    mode: 'production',
+    output: {
+      filename: '[name].[hash:8].js',
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          extractComments: true,
+          parallel: true,
+        }),
+        new CssMinimizerPlugin({
+          minimizerOptions: {
+            preset: [
+              'default',
+              {
+                discardComments: { removeAll: true },
+              },
+            ],
+          },
+        }),
+      ],
+    },
+  };
 
-	return merge([configFromCommon, result])
-}
+  return merge([configFromCommon, result]);
+};
