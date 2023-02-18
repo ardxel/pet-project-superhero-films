1e7d3c14963cb147517715964fd2783bd84afc73
ardxel <ardxel@gmail.com>
2023-02-18 21:55:39 +0300

    api for movies

---
+++ src/redux/actions/moviesApi.ts
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import BASE_URL from '@constants/baseUrl';
+import IMovie from 'types/Movie';
+
+export const moviesApi = createApi({
+  reducerPath: 'movies',
+  baseQuery: fetchBaseQuery({baseUrl: BASE_URL }),
+  endpoints: (builder) =>  ({
+    getMovies: builder.query<IMovie[], string>({
+      query: () => '/movies-all'
+    }),
+    getFranchise: builder.query<IMovie[], string>({
+      query: (name) => `franchise=${name}`
+    })
+
+  })
+})
+
+export const {useGetFranchiseQuery, useGetMoviesQuery} = moviesApi;
