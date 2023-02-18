b2e0f46688e9b2d91edfaf2a28bdefe38622e6e7
ardxel <ardxel@gmail.com>
2023-02-18 21:37:16 +0300

    function to get theme from local storage

---
+++ src/tools/getTheme.ts
@@ -0,0 +1,11 @@
+export const getTheme = function <S extends 'light' | 'dark'>(themes: { [key: string]: S }): S {
+  // The item 'theme' is checked in local storage.
+  // If there is, it is returned
+  const theme = JSON.parse(`${window.localStorage.getItem('theme')}`);
+  if (Object.values(themes).includes(theme)) return theme;
+
+  const userMedia = window.matchMedia('(prefers-color-scheme: light)');
+  if (userMedia.matches) return themes.light;
+  return themes.dark;
+};
+
