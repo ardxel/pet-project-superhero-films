769c9a4474c3a62924ce3b47381196564ccb1a18
ardxel <ardxel@gmail.com>
2023-02-22 01:14:20 +0300

    selection of films for sliders

---
+++ src/constants/franchisesList.ts
@@ -0,0 +1,9 @@
+const franchisesList = [
+  {id: 1, name: 'Spider-Man'},
+  {id: 2, name: 'Batman'},
+  {id: 3, name: 'X-Men'},
+  {id: 4, name: 'Avengers'},
+  {id: 5, name: 'Superman'}
+]
+
+export default franchisesList;
