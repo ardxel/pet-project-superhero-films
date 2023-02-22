44ec3b8a6a0d75cd0e6d7d605fb59ff317586d7e
Vasily Bobnev <ardxel@gmail.com>
2023-02-23 02:00:06 +0300

    Update store.ts
    changed main reducer in store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,20 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { moviesApi } from 'redux/actions/moviesApi';
-import {newsApi} from 'redux/actions/newsApi';
-
+import newsSlice from 'redux/reducers/newsReducer';
 
 const store = configureStore({
   reducer: {
     [moviesApi.reducerPath]: moviesApi.reducer,
-    [newsApi.reducerPath]: newsApi.reducer
+    [newsSlice.name]: newsSlice.reducer,
   },
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(newsApi.middleware, moviesApi.middleware)
-  }
+    return getDefaultMiddleware().concat(moviesApi.middleware);
+  },
 });
 
 export default store;
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
-
