a7612558d4a7a5fddb1930b9c22399697dd92b28
Vasily Bobnev <ardxel@gmail.com>
2023-02-23 01:56:15 +0300

    Update tailwind.config.js
    new screens and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['.src/**/*.{scss,css,tsx}'],
+  content: ['.src/**/**/*.{scss,css,tsx}'],
   theme: {
     extend: {
       screens: {
-        'xl': '1176px',
-      }
+        sm: '500px',
+        md: '618px',
+        nl: '736px',
+        lg: '900px',
+        xl: '1176px',
+      },
+      colors: {
+        color1: 'var(--color1)',
+        color39: 'var(--color39)',
+        color97: 'var(--color97)',
+      },
     },
   },
   plugins: [],
-}
+};
