561f757a6c11fbb109420120c6315b920706428b
Vasily Bobnev <ardxel@gmail.com>
2023-02-23 01:55:33 +0300

    Update App.tsx
    prettier fix

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,19 @@ import Home from '@pages/home/Home';
 import News from '@pages/news/News';
 import { Provider } from 'react-redux';
 import store from 'redux/store';
-const url = ' https://newsdata.io/api/1/news?apikey=pub_172429e9645c6b1e1b927666cf8ad362b7fb6&q=marvel';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Provider store={store}>
         <Routes>
-          <Route path='/' element={<Header />}>
+          <Route path="/" element={<Header />}>
             <Route index element={<Home />} />
-            <Route path='news' element={<News />}/>
-            <Route path='root' element={<Admin />} />
+            <Route path="news" element={<News />} />
+            <Route path="root" element={<Admin />} />
           </Route>
         </Routes>
       </Provider>
-
     </BrowserRouter>
   );
 };
