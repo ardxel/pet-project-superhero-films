36eb8539464d1c730af8891c5fa7fd1ee0cd6aa3
Vasily Bobnev <ardxel@gmail.com>
2023-02-27 01:47:16 +0300

    Update upgradeRating.ts
    new function

--- src/tools/upgradeRating.ts
+++ src/tools/upgradeRating.ts
@@ -1,13 +1,11 @@
-const upgradeRating = (rating: number): [string, string] => {
-  return [
-    getColor(rating),
-    String(rating).length === 1 ? String(rating) + '.0' : String(rating),
-  ];
+export const upgradeRating = (rating: number): [string, string] => {
+  return [getColor(rating), getRating(rating)];
 };
 
-export default upgradeRating;
-
-const getColor = (num: number): string => {
+export const getRating = (num: number): string => {
+  return String(num).length === 1 ? num + '.0': String(num);
+}
+export const getColor = (num: number): string => {
   if (num >= 7) return '#2CA900';
   else if (num >= 6 && num < 7) return '#CA740C';
   else if (num >= 5 && num < 6) return '#F82D2D';
