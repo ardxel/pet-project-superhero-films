38fb840f0ab5f2047ef8b5ac994b9e447485144f
Vasily Bobnev <ardxel@gmail.com>
2023-02-28 09:15:18 +0300

    Update franchisesList.ts
    new interfaces

--- src/constants/franchisesList.ts
+++ src/constants/franchisesList.ts
@@ -1,9 +1,20 @@
-const franchisesList = [
-  {id: 1, name: 'Spider-Man'},
-  {id: 2, name: 'Batman'},
-  {id: 3, name: 'X-Men'},
-  {id: 4, name: 'Avengers'},
-  {id: 5, name: 'Superman'}
-]
+import IMovie from 'types/Movie';
+
+export interface FranchiseList {
+  id: number;
+  keywords?: string[];
+}
+export interface FranchiseListResponse extends FranchiseList {
+  title: string;
+  list: IMovie[];
+}
+
+const franchisesList: FranchiseList[] = [
+  { id: 1, keywords: ['Spider-Man'] },
+  { id: 2, keywords: ['Batman', 'The Dark Knight'] },
+  { id: 3, keywords: ['X-Men', 'The Wolverine', 'Logan'] },
+  { id: 4, keywords: ['Avengers'] },
+  { id: 5, keywords: ['Superman', 'Man of Steel'] },
+];
 
 export default franchisesList;
