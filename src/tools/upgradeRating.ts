0fb7d536bc443e5b34db1a83f151d541f72613be
Vasily Bobnev <ardxel@gmail.com>
2023-03-01 06:21:31 +0300

    Update upgradeRating.ts
    prettier fix

--- src/tools/upgradeRating.ts
+++ src/tools/upgradeRating.ts
@@ -3,8 +3,8 @@ export const upgradeRating = (rating: number): [string, string] => {
 };
 
 export const getRating = (num: number): string => {
-  return String(num).length === 1 ? num + '.0': String(num);
-}
+  return String(num).length === 1 ? num + '.0' : String(num);
+};
 export const getColor = (num: number): string => {
   if (num >= 7) return '#2CA900';
   else if (num >= 6 && num < 7) return '#CA740C';
