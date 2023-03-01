b1653eaee74d7c2bbe41e9175ef158e135d11368
Vasily Bobnev <ardxel@gmail.com>
2023-03-01 06:20:09 +0300

    prettier fix
    prettier fix

--- src/components/header/buttons/LogoButton.tsx
+++ src/components/header/buttons/LogoButton.tsx
@@ -3,16 +3,16 @@ import ButtonType from '@components/header/buttons/ButtonType';
 import { Link } from 'react-router-dom';
 import { LogoSVG } from '@images/index';
 
-interface  LogoButtonProps extends ButtonType {}
+interface LogoButtonProps extends ButtonType {}
 
-const LogoButton: React.FC<LogoButtonProps> = ({className, link}) => {
+const LogoButton: React.FC<LogoButtonProps> = ({ className, link }) => {
   return (
     <button className={className}>
       <Link to={link || '/'}>
-        <LogoSVG/>
+        <LogoSVG />
       </Link>
     </button>
-  )
-}
+  );
+};
 
 export default LogoButton;
--- src/components/header/buttons/ThemeButton.tsx
+++ src/components/header/buttons/ThemeButton.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import ButtonType from '@components/header/buttons/ButtonType';
-import {ThemeSVG} from '@images/index';
+import { ThemeSVG } from '@images/index';
 
 interface ThemeButtonProps extends ButtonType {}
 
-const ThemeButton: React.FC<ThemeButtonProps> = ({className, onClick}) => {
+const ThemeButton: React.FC<ThemeButtonProps> = ({ className, onClick }) => {
   return (
-    <button className={className as string}
-            onClick={onClick}>
-      <ThemeSVG/>
+    <button className={className as string} onClick={onClick}>
+      <ThemeSVG />
     </button>
-  )
-}
+  );
+};
 
 export default ThemeButton;
