0979ff4c673510c377387a312c7cbbac0a5c4b21
Vasily Bobnev <ardxel@gmail.com>
2023-03-02 15:23:56 +0300

    Create Base.tsx

---
+++ src/pages/movie-page/header/base/Base.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './base.module.scss';
+import toHoursAndMinutes from 'tools/toHoursAndMinutes';
+import formatAgeLimits from 'tools/formatAgeLimits';
+
+interface BaseProps {
+  nameOriginal: string;
+  nameRu: string;
+  year: number;
+  filmLength: number;
+  ratingAgeLimits: string;
+}
+
+const Base: React.FC<BaseProps> = ({
+  nameOriginal,
+  nameRu,
+  year,
+  filmLength,
+  ratingAgeLimits,
+}) => {
+  return (
+    <div className={styles.base}>
+      <div className={styles.title}>
+        <h3>{`${nameOriginal} / ${nameRu}`}</h3>
+      </div>
+      <div className={styles.params}>
+        <span className={styles.year}>{year}</span>
+        <span className={styles.length}>{toHoursAndMinutes(filmLength)}</span>
+        <span className={styles.age}>{formatAgeLimits(ratingAgeLimits)}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Base;
