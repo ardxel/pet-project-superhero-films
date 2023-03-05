ad47ece6f087399d240d6102131c7a88044bf5d7
Vasily Bobnev <ardxel@gmail.com>
2023-03-05 20:48:36 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import '@styles/main.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Header from './components/header/Header';
+import Header from '@components/header/Header';
+import Footer from '@components/footer/Footer';
 import Admin from '@pages/root/Admin';
 import Home from '@pages/home/Home';
 import News from '@pages/news/News';
 import MoviePage from '@pages/movie-page/MoviePage';
+import SignUp from '@pages/signup/SignUp';
 import { Provider } from 'react-redux';
 import store from 'redux/store';
 
@@ -13,14 +15,15 @@ const App = () => {
   return (
     <BrowserRouter>
       <Provider store={store}>
+        <Header />
         <Routes>
-          <Route path="/" element={<Header />}>
-            <Route index element={<Home />} />
-            <Route path="news" element={<News />} />
-            <Route path="/movie/:id" element={<MoviePage />} />
-            <Route path="root" element={<Admin />} />
-          </Route>
+          <Route index element={<Home />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/movie/:id" element={<MoviePage />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="root" element={<Admin />} />
         </Routes>
+        <Footer />
       </Provider>
     </BrowserRouter>
   );
