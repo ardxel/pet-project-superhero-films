f64744f4aa56181fc119a38b73ba4f45354eb6b1
Vasily Bobnev <ardxel@gmail.com>
2023-03-05 20:49:05 +0300

    Update CardMovie.tsx

--- src/components/card-components/card-movie/CardMovie.tsx
+++ src/components/card-components/card-movie/CardMovie.tsx
@@ -1,33 +1,27 @@
 import React from 'react';
 import styles from './cardMovie.module.scss';
-import { upgradeRating } from 'tools/upgradeRating';
-import toHoursAndMinutes from 'tools/toHoursAndMinutes';
+import { upgradeRating } from 'common/tools/upgradeRating';
+import toHoursAndMinutes from 'common/tools/toHoursAndMinutes';
 import { Link } from 'react-router-dom';
+import IMovie from 'types/Movie';
 
-interface CardMovieProps {
-  id: number;
-  rating: number;
-  year: number;
-  posterUrl: string;
-  name: string;
-  filmLength: number;
-}
+interface CardMovieProps extends IMovie {}
 
 const CardMovie: React.FC<CardMovieProps> = ({
-  id,
-  rating,
+  kinopoiskId,
+  ratingKinopoisk,
   year,
   posterUrl,
-  name,
+  nameOriginal,
   filmLength,
 }) => {
-  const [color, newRating] = upgradeRating(rating);
+  const [color, newRating] = upgradeRating(ratingKinopoisk);
 
   return (
     <li className={styles.movie}>
       <div className={styles.container}>
-        <Link to={`/movie/${id}`}>
-          <img src={posterUrl} alt={name} />
+        <Link to={`/movie/${kinopoiskId}`}>
+          <img src={posterUrl} alt={nameOriginal} />
           <div className={styles.info}>
             <span className={styles.rating} style={{ background: color }}>
               {newRating}
