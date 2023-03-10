dba56b5ace9b9707240501cfe8c58f0c30b4bfe0
Vasily Bobnev <ardxel@gmail.com>
2023-03-10 22:10:22 +0300

    Update store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,30 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { moviesApi } from 'redux/actions/moviesApi';
+import { userApi } from 'redux/actions/userApi';
 import newsSlice from 'redux/reducers/newsReducer';
+import userSlice from 'redux/reducers/userReducer';
+import { listenerMiddleware } from 'redux/middleware';
+import { preloadUserReduxState } from 'redux/actions/loadUserStateData';
 
 const store = configureStore({
   reducer: {
-    [moviesApi.reducerPath]: moviesApi.reducer,
+    [userSlice.name]: userSlice.reducer,
     [newsSlice.name]: newsSlice.reducer,
+    [userApi.reducerPath]: userApi.reducer,
+    [moviesApi.reducerPath]: moviesApi.reducer
   },
-  middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(moviesApi.middleware);
-  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(
+    listenerMiddleware.middleware,
+    moviesApi.middleware,
+    userApi.middleware
+  )
 });
+
+const getUserToken = () => {
+  return JSON.parse(localStorage.getItem('user') || 'null');
+};
+
+store.dispatch(preloadUserReduxState(getUserToken()));
 
 export default store;
 export type RootState = ReturnType<typeof store.getState>;
