2af8897d0ac8d92808e830a33f9c26e7d7bdfa0c
Vasily Bobnev <ardxel@gmail.com>
2023-03-12 21:26:50 +0300

    Delete getShuffleArray.ts

--- src/common/tools/getShuffleArray.ts
+++
@@ -1,7 +0,0 @@
-export default function getShuffleArray<T extends Array<any>>(arr: T): T {
-  if (arr.length === 1) {
-    return arr;
-  }
-  const rand = Math.floor(Math.random() * arr.length);
-  return [arr[rand], ...getShuffleArray(arr.filter((_, i) => i != rand))] as T;
-}
