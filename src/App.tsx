a268cd64db5c81b901678ab390aa3dad1e93a831
Vasily Bobnev <ardxel@gmail.com>
2023-03-12 21:27:55 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import '@styles/main.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ScrollToTop from 'common/scroll-to-top/ScrollToTop';
 import Header from '@components/header/Header';
 import Footer from '@components/footer/Footer';
 import Admin from '@pages/root/Admin';
 import Home from '@pages/home/Home';
 import News from '@pages/news/News';
 import MoviePage from '@pages/movie-page/MoviePage';
-import SignUp from '@pages/signup/SignUp';
+import Authorization from '@pages/authorization/Authorization';
 import { Provider } from 'react-redux';
 import store from 'redux/store';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Provider store={store}>
         <Header />
         <Routes>
           <Route index element={<Home />} />
           <Route path="/news" element={<News />} />
           <Route path="/movie/:id" element={<MoviePage />} />
-          <Route path="/signup" element={<SignUp />} />
+          <Route path="/authorization" element={<Authorization />} />
           <Route path="root" element={<Admin />} />
         </Routes>
         <Footer />
