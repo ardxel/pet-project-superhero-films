402feaf12c6478359c8d3e5b93ed7cf980a8f06e
Vasily Bobnev <ardxel@gmail.com>
2023-03-13 23:41:14 +0300

    Create index.ts

---
+++ src/common/tools/index.ts
@@ -0,0 +1,18 @@
+import formatAgeLimits from 'common/tools/formatAgeLimits';
+import formatDateInArray from 'common/tools/formatDateInArray';
+import getTheme from 'common/tools/getTheme';
+import getTokenFromLocalStorage from 'common/tools/getTokenFromLocalStorage';
+import { disableScroll, enableScroll } from 'common/tools/scroll-lock';
+import toHoursAndMinutes from 'common/tools/toHoursAndMinutes';
+import { upgradeRating } from 'common/tools/upgradeRating';
+
+export {
+  formatDateInArray,
+  enableScroll,
+  disableScroll,
+  formatAgeLimits,
+  getTheme,
+  upgradeRating,
+  getTokenFromLocalStorage,
+  toHoursAndMinutes,
+};
