38a3ca93c4a2d4dcdbaf11938f22ae197aa03f2c
Vasily Bobnev <ardxel@gmail.com>
2023-03-23 22:57:55 +0300

    Update navLinks.ts

--- src/constants/navLinks.ts
+++ src/constants/navLinks.ts
@@ -1,13 +1,9 @@
-const navLinks: Array<{
-  readonly id: number;
-  readonly name: string;
-  readonly path: string;
-}> = [
+const navLinks = [
   { id: 0, name: 'Home', path: '/' },
   { id: 1, name: 'News', path: '/news' },
   { id: 2, name: 'Marvel', path: '' },
   { id: 3, name: 'DC', path: '' },
-  { id: 4, name: 'Sign in', path: '' },
+  { id: 4, name: 'Sign in', path: '/authorization' },
 ];
 
 export default navLinks;
