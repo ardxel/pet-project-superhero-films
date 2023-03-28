72b0f4e23ced44d2fc0b0777a2c7229c429c010a
Vasily Bobnev <ardxel@gmail.com>
2023-03-28 22:34:13 +0300

    Update FormTitle.tsx

--- src/components/forms/form-title/FormTitle.tsx
+++ src/components/forms/form-title/FormTitle.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { memo, useCallback, useEffect, useState } from 'react';
 import styles from './formtitle.module.scss';
 import { Alert, AlertColor, AlertTitle } from '@mui/material';
 import { sleep } from 'common/tools';
@@ -12,31 +12,39 @@ interface FormTitleProps {
 
 const FormTitle: React.FC<FormTitleProps> = ({ ...props }) => {
   const { showAlert, severity, title, message } = props;
-  const [showAlertMessage, setShowAlertMessage] = useState(true);
+  const [showAlertMessage, setShowAlertMessage] = useState(false);
   useEffect(() => {
     if (showAlert) {
-      sleep(5000).then(() => setShowAlertMessage(false));
+      handleShowAlertMessage();
     }
+    let alertTimeout = setTimeout(handleHideAlertMessage, 3000);
+    return () => {
+      clearTimeout(alertTimeout);
+    };
   }, [showAlert]);
 
-  if ((!showAlert && showAlertMessage) || (showAlert && !showAlertMessage)) {
-    return (
-      <div className={styles.title}>
-        <h3>{title}</h3>
-      </div>
-    );
-  } else if (!title) {
+  const handleShowAlertMessage = useCallback(() => {
+    setShowAlertMessage(true);
+  }, [showAlertMessage]);
+
+  const handleHideAlertMessage = useCallback(() => {
+    setShowAlertMessage(false);
+  }, [showAlertMessage]);
+
+  if (!title) {
     return null;
   } else
     return (
       <div className={styles.title}>
         <h3>{title}</h3>
-        <Alert className={styles.alert} severity={severity}>
-          <AlertTitle>{severity}</AlertTitle>
-          {message}
-        </Alert>
+        {showAlertMessage && (
+          <Alert className={styles.alert} severity={severity}>
+            <AlertTitle>{severity}</AlertTitle>
+            {message}
+          </Alert>
+        )}
       </div>
     );
 };
 
-export default FormTitle;
+export default memo(FormTitle);
