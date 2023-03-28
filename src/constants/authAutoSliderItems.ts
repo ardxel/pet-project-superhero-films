62b2e4df40baeeab802ef2106efa995a5599e7cf
Vasily Bobnev <ardxel@gmail.com>
2023-03-28 22:35:25 +0300

    Create authAutoSliderItems.ts

---
+++ src/constants/authAutoSliderItems.ts
@@ -0,0 +1,10 @@
+export default [
+  {
+    image: 'https://wallpaperaccess.com/full/7978003.jpg',
+    title: 'Watch various selections of movies online!',
+  },
+  {
+    image: 'https://i.ytimg.com/vi/cQ5Cp_kdQDU/maxresdefault.jpg',
+    title: 'Check out the latest news section!',
+  },
+];
