f7474cfbe5160e77db34acd12641fc91496b61c7
Vasily Bobnev <ardxel@gmail.com>
2023-03-29 23:48:28 +0300

    Update User.ts

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,18 +1,31 @@
-import Movie from './/Movie';
-
 export type UserToken = string | null;
 
-export interface UserReduxState {
+export type UserCollection = {
+  favorites: number[];
+  ratings: { id: number; value: number }[];
+  watchlist: number[];
+};
+
+export type UserPersonalInfo = {
+  gender: string | null;
+  birthday: string | null;
+  country: string | null;
+};
+
+export interface UserReduxState extends UserCollection, UserPersonalInfo {
+  username: string | null;
+  name: string | null;
   token: UserToken | null;
   avatar: string | null;
-  favorites: Movie[];
+  biography: string | null;
 }
 
 export interface UserServerState extends UserReduxState {
   token: UserToken;
-  avatar: string;
+  avatar: string | null;
   email: string;
   username: string;
+  name: string | null;
   password: string;
   isAdmin: boolean;
 }
