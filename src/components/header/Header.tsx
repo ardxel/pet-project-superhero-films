a39fbf20b2a2514d23abdc3d0b46f5f2ece77754
Vasily Bobnev <ardxel@gmail.com>
2023-03-29 23:48:03 +0300

    Update Header.tsx

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './header.module.scss';
 import muiStyles from 'common/formFields/styles';
 import Navbar from './navbar/Navbar';
@@ -13,19 +13,19 @@ import {
 } from './buttons/index';
 import SearchIcon from '@mui/icons-material/Search';
 import CloseIcon from '@mui/icons-material/Close';
-import { Menu, MenuItem } from '@mui/material';
-import { useAppSelector } from '@hooks/useAppSelector';
+import { Menu, MenuItem, Paper } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useAppDispatch } from '@hooks/useAppDispatch';
 import { logout } from 'redux/reducers/userReducer';
 import { sleep } from 'common/tools';
 import { useNavigate } from 'react-router';
+import useUserProfile from '@hooks/useUserProfile';
 
 const Header = () => {
   const [isNavbarOpen, setIsNavbarOpen] = useState<boolean>(false);
   const [isSearchOpen, setIsSearchOpen] = useState<boolean>(false);
   const [anchorElem, setAnchorElem] = useState<HTMLButtonElement | null>(null);
-  const { token, username } = useAppSelector((state) => state.user);
+  const { userState, isAuthorized } = useUserProfile();
   const navigate = useNavigate();
   const { changeTheme } = useTheme();
   const dispatch = useAppDispatch();
@@ -57,82 +57,87 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <LogoButton className={styles.logo} link={'/'} />
+        <Paper
+          elevation={6}
+          sx={{ height: '100%', backgroundColor: 'inherit' }}
+        >
+          <LogoButton className={styles.logo} link={'/'} />
 
-        <Navbar
-          username={username || null}
-          isOpen={isNavbarOpen}
-          setIsOpen={setIsNavbarOpen}
-        />
-        <Search isOpen={isSearchOpen} setIsOpen={setIsSearchOpen} />
-
-        <div className={styles.interface}>
-          <SearchButton className={styles.search} onClick={showSearch}>
-            {isSearchOpen ? <CloseIcon /> : <SearchIcon />}
-          </SearchButton>
-
-          <ThemeButton className={styles.theme} onClick={changeTheme} />
-          <UserButton
-            id="basic-menu-button"
-            className={[styles.userMenu, '.mui-fixed'].join(' ')}
-            aria-owns={anchorElem ? 'userMenu' : undefined}
-            aria-haspopup={true}
-            onClick={showUserMenu}
-            onMouseOver={showUserMenu}
+          <Navbar
+            username={userState.username || null}
+            isOpen={isNavbarOpen}
+            setIsOpen={setIsNavbarOpen}
           />
+          <Search isOpen={isSearchOpen} setIsOpen={setIsSearchOpen} />
 
-          <Menu
-            id="userMenu"
-            anchorEl={anchorElem}
-            disableScrollLock={true}
-            sx={muiStyles.menu}
-            open={!!anchorElem}
-            onClose={closeUserMenu}
-            anchorOrigin={{
-              vertical: 'bottom',
-              horizontal: 'center',
-            }}
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'center',
-            }}
-            MenuListProps={{
-              'aria-labelledby': 'basic-menu-button',
-              onMouseLeave: closeUserMenu,
-            }}
-          >
-            {token && (
-              <MenuItem>
-                <Link
-                  style={{ color: 'var(--color13)' }}
-                  to={`profile/${username}`}
-                >
-                  Profile
-                </Link>
-              </MenuItem>
-            )}
-            {!token && (
-              <MenuItem>
-                <Link style={{ color: 'var(--color13)' }} to="/authorization">
-                  Sign up
-                </Link>
-              </MenuItem>
-            )}
-            {token && (
-              <MenuItem>
-                <Link
-                  style={{ color: 'var(--color13)' }}
-                  to=""
-                  onClick={handleLogout}
-                >
-                  Logout
-                </Link>
-              </MenuItem>
-            )}
-          </Menu>
+          <div className={styles.interface}>
+            <SearchButton className={styles.search} onClick={showSearch}>
+              {isSearchOpen ? <CloseIcon /> : <SearchIcon />}
+            </SearchButton>
 
-          <MenuButton className={styles.menu} onClick={showNavbar} />
-        </div>
+            <ThemeButton className={styles.theme} onClick={changeTheme} />
+            <UserButton
+              id="basic-menu-button"
+              className={[styles.userMenu, '.mui-fixed'].join(' ')}
+              aria-owns={anchorElem ? 'userMenu' : undefined}
+              aria-haspopup={true}
+              onClick={showUserMenu}
+              onMouseOver={showUserMenu}
+            />
+
+            <Menu
+              id="userMenu"
+              anchorEl={anchorElem}
+              disableScrollLock={true}
+              sx={muiStyles.menu}
+              open={!!anchorElem}
+              onClose={closeUserMenu}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'center',
+              }}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'center',
+              }}
+              MenuListProps={{
+                'aria-labelledby': 'basic-menu-button',
+                onMouseLeave: closeUserMenu,
+              }}
+            >
+              {isAuthorized && (
+                <MenuItem>
+                  <Link
+                    style={{ color: 'var(--color13)' }}
+                    to={`profile/${userState.username}`}
+                  >
+                    Profile
+                  </Link>
+                </MenuItem>
+              )}
+              {!isAuthorized && (
+                <MenuItem>
+                  <Link style={{ color: 'var(--color13)' }} to="/authorization">
+                    Sign up
+                  </Link>
+                </MenuItem>
+              )}
+              {isAuthorized && (
+                <MenuItem>
+                  <Link
+                    style={{ color: 'var(--color13)' }}
+                    to=""
+                    onClick={handleLogout}
+                  >
+                    Logout
+                  </Link>
+                </MenuItem>
+              )}
+            </Menu>
+
+            <MenuButton className={styles.menu} onClick={showNavbar} />
+          </div>
+        </Paper>
       </div>
     </header>
   );
