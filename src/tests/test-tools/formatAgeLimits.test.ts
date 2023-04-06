33184dbfe6e8e2d5f6d7cd1a7c7815b78a3b360e
Vasily Bobnev <ardxel@gmail.com>
2023-04-06 05:31:36 +0300

    Update formatAgeLimits.test.ts

--- src/tests/test-tools/formatAgeLimits.test.ts
+++ src/tests/test-tools/formatAgeLimits.test.ts
@@ -1,10 +1,10 @@
-import formatAgeLimits from "@common/tools/formatAgeLimits";
+import formatAgeLimits from '@common/tools/formatAgeLimits';
 
-describe("formatAgeLimits module", () => {
+describe('formatAgeLimits module', () => {
   test('get "pg16" return "16+"', () => {
-    expect(formatAgeLimits("pg16")).toBe("16+");
+    expect(formatAgeLimits('pg16')).toBe('16+');
   });
   test('get "qwerty12345" return "12345+"', () => {
-    expect(formatAgeLimits("qwerty12345")).toBe("12345+");
+    expect(formatAgeLimits('qwerty12345')).toBe('12345+');
   });
 });
