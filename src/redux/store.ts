d17255c8f38d197cc413b2672210f91b5bb744c9
Vasily Bobnev <ardxel@gmail.com>
2023-04-07 21:26:38 +0300

    Update store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,25 +1,37 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { moviesApi } from 'redux/api/moviesApi';
-import { userApi } from 'redux/api/userApi';
-import newsSlice from 'redux/reducers/newsReducer';
-import userSlice from 'redux/reducers/userReducer';
-import { listenerMiddleware } from 'redux/middleware';
-import { preloadUserReduxState } from 'redux/asyncThunks/userThunks';
-import { getTokenFromLocalStorage } from 'common/tools';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+  moviesApiMiddleware,
+  moviesApiName,
+  moviesApiReducer,
+} from '@reduxproj/api/moviesApi';
+import {
+  userApiMiddleware,
+  userApiName,
+  userApiReducer,
+} from '@reduxproj/api/userApi';
+import { newsReducer } from '@reduxproj/reducers/newsReducer';
+import { userReducer } from '@reduxproj/reducers/userReducer';
+import listenerMiddleware from '@reduxproj/middleware';
+import { preloadUserReduxState } from '@reduxproj/asyncThunks/userThunks';
+import getTokenFromLocalStorage from '@tools/getTokenFromLocalStorage';
+
+export const rootReducer = combineReducers({
+  user: userReducer,
+  news: newsReducer,
+  [userApiName]: userApiReducer,
+  [moviesApiName]: moviesApiReducer,
+});
+
+export const middlewareCombiner = [
+  listenerMiddleware,
+  moviesApiMiddleware,
+  userApiMiddleware,
+];
 
 const store = configureStore({
-  reducer: {
-    [userSlice.name]: userSlice.reducer,
-    [newsSlice.name]: newsSlice.reducer,
-    [userApi.reducerPath]: userApi.reducer,
-    [moviesApi.reducerPath]: moviesApi.reducer,
-  },
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(
-      listenerMiddleware.middleware,
-      moviesApi.middleware,
-      userApi.middleware
-    ),
+    getDefaultMiddleware().concat(...middlewareCombiner),
 });
 
 (() => {
@@ -30,5 +42,6 @@ const store = configureStore({
 })();
 
 export default store;
+export type AppStore = typeof store;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
