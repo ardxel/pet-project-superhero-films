fa0b652def1b8073916637fbb2e6bfa17c38c1f6
Vasily Bobnev <ardxel@gmail.com>
2023-04-08 21:58:24 +0300

    new item for auth page auto slider

--- src/constants/authAutoSliderItems.ts
+++ src/constants/authAutoSliderItems.ts
@@ -7,4 +7,10 @@ export default [
     image: 'https://i.ytimg.com/vi/cQ5Cp_kdQDU/maxresdefault.jpg',
     title: 'Check out the latest news section!',
   },
+  {
+    image:
+      'https://cs8.pikabu.ru/post_img/big/2016/05/08/9/1462722351198872397.jpg',
+    title:
+      'add movies to your watchlist so that you can easily find this movie in your bookmarks',
+  },
 ];
