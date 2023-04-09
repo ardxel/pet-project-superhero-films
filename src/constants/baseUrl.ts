7e016d5f702dfb0fdf5dafc981397e1c22d80009
Vasily Bobnev <ardxel@gmail.com>
2023-04-09 22:39:39 +0300

    change base url v3000

--- src/constants/baseUrl.ts
+++ src/constants/baseUrl.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://superhero-films-express-vercel-ardxel.vercel.app';
+const BASE_URL = 'https://superhero-films-express-vercel.vercel.app';
 
 // use this variable if you want to test project in local network
 // const BASE_URL = 'http://localhost:3000/3000'
