99acfa35758d8638bc73d138f14bf77385e04c54
Vasily Bobnev <ardxel@gmail.com>
2023-04-09 18:59:28 +0300

    Update baseUrl.ts

--- src/constants/baseUrl.ts
+++ src/constants/baseUrl.ts
@@ -1,4 +1,2 @@
-// const BASE_URL = 'http://192.168.0.103:5000';
-const BASE_URL = 'superhero-films-git-dev-ardxel.vercel.app'
-// const BASE_URL = 'http://127.0.0.1:5000';
+const BASE_URL = 'https://superhero-films-express-vercel.vercel.app/'
 export default BASE_URL;
