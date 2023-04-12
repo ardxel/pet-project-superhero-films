9684b9d3abe21b5c43000a1f413208465378e1f6
Vasily Bobnev <ardxel@gmail.com>
2023-04-12 22:18:50 +0300

    add new prop for background image

--- src/common/wrapper/Wrapper.tsx
+++ src/common/wrapper/Wrapper.tsx
@@ -4,12 +4,21 @@ import { Paper } from '@mui/material';
 
 interface WrapperProps {
   children: React.ReactNode;
+  backgroundImage?: string
 }
 
-const Wrapper: React.FC<WrapperProps> = ({ children }) => {
+const Wrapper: React.FC<WrapperProps> = ({ children, backgroundImage }) => {
+
   return (
     <main className={styles.main} data-testid="main-wrapper">
       <div className={styles.container}>
+        {backgroundImage && (
+          <div className={styles.bg}>
+            <div className={styles.img} style={{backgroundImage: `url(${backgroundImage})`}}>
+              <div className={styles.gradient}></div>
+            </div>
+          </div>
+        )}
         <Paper elevation={3} sx={{ backgroundColor: 'inherit' }}>
           {children}
         </Paper>
