244c68fcf9361a900d1c1d79f23e8b9e8cb7bef3
Vasily Bobnev <ardxel@gmail.com>
2023-04-13 23:52:38 +0300

    Fundamental change in the logic of the component.
    Changing the display of elements thanks to React.Children

--- src/components/sliders/auto-slider/AutoSlider.tsx
+++ src/components/sliders/auto-slider/AutoSlider.tsx
@@ -1,69 +1,80 @@
-import React, { memo, useEffect, useState } from 'react';
+import React, {
+  memo, ReactElement, ReactNode,
+  Children,
+  cloneElement,
+  useEffect,
+  useState
+} from 'react';
 import styles from './autoslider.module.scss';
 
 const sliderStyles = {
   activeSlide: {
     opacity: '1',
-    transform: 'translateX(0)',
+    transform: 'translateX(0)'
   },
   lastSlide: {
-    transform: 'translateX(-100%)',
+    transform: 'translateX(-100%)'
   },
   nextSlide: {
-    transform: 'translateX(100%)',
-  },
+    transform: 'translateX(100%)'
+  }
 };
 
-type AutoSliderDataItem = { image: string; title: string };
+type slideWithOpacity = { opacity?: string, transform: string }
 
 interface AutoSliderProps {
-  data: AutoSliderDataItem[];
+  speed?: number;
+  children: ReactNode | ReactNode[];
 }
 
-const AutoSlider: React.FC<AutoSliderProps> = ({ data }) => {
+const AutoSlider: React.FC<AutoSliderProps> = ({ children, speed }) => {
   const [index, setIndex] = useState(0);
+  const [length, setLength] = useState((children as ReactNode[]).length);
+  const defaultIntervalSpeed = 5000;
   useEffect(() => {
     if (index < 0) {
-      setIndex(data.length - 1);
+      setIndex(length - 1);
     }
-    if (index > data.length - 1) {
+    if (index > length - 1) {
       setIndex(0);
     }
-  }, [index, data]);
+  }, [index]);
 
   useEffect(() => {
     let slider = setInterval(() => {
       setIndex(index + 1);
-    }, 5000);
+    }, speed ? speed : defaultIntervalSpeed);
     return () => {
       clearInterval(slider);
     };
   }, [index]);
+
   return (
     <div className={styles.section}>
       <div className={styles.menu}>
-        {data.map((item, itemIndex) => {
-          const { image, title } = item;
-          let position = sliderStyles.nextSlide;
-          if (itemIndex === index) {
+        {Children.map(children, (child, childIndex) => {
+
+          let position: slideWithOpacity = sliderStyles.nextSlide;
+          if (childIndex === index) {
             position = sliderStyles.activeSlide;
           }
           if (
-            itemIndex === index - 1 ||
-            (index === 0 && itemIndex === data.length - 1)
+            childIndex === index - 1 ||
+            (index === 0 && childIndex === length - 1)
           ) {
             position = sliderStyles.lastSlide;
           }
-          return (
-            <div className={styles.item} style={position} key={itemIndex}>
-              <div className={styles.title}>
-                <h6>{title}</h6>
-              </div>
-              <div className={styles.image}>
-                <img src={image} />
-              </div>
-            </div>
-          );
+
+          return cloneElement(child as ReactElement, {
+            style: {
+              ...position,
+              opacity: position.opacity ? position.opacity : 0,
+              transition: `all 0.3s linear`,
+              position: 'absolute',
+              width: '100%',
+              height: '100%'
+            }
+          });
         })}
       </div>
     </div>
