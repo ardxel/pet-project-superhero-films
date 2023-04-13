06996faa5cd1f4751468e2a4dc643d633dd312ef
Vasily Bobnev <ardxel@gmail.com>
2023-04-13 23:51:25 +0300

    change search card movie component

--- src/components/search/search-card-movie/SearchCardMovie.tsx
+++ src/components/search/search-card-movie/SearchCardMovie.tsx
@@ -1,27 +1,26 @@
 import React, { FC, memo } from 'react';
 import styles from './seachCarMovie.module.scss';
 import { Link } from 'react-router-dom';
-import useMovieReview from '@hooks/useMovieReview';
 import IMovie from '@models/Movie';
 
 interface SearchCardMovieProps extends IMovie {
   setIsOpen: (arg: boolean) => void;
 }
 
-const SearchCardMovie: FC<SearchCardMovieProps> = ({...props}) => {
-
-  const {isFavorite, isInWatchlist, isInRatings} = useMovieReview(props.kinopoiskId);
-
-
-
+const SearchCardMovie: FC<SearchCardMovieProps> = ({ ...props }) => {
   return (
-    <Link to={`/movie/${props.kinopoiskId}`} onClick={props.setIsOpen.bind(null, false)}>
+    <Link
+      to={`/movie/${props.kinopoiskId}`}
+      onClick={props.setIsOpen.bind(null, false)}
+    >
       <article className={styles.item}>
         <div className={styles.itemImg}>
           <img src={props.posterUrl} alt={props.nameOriginal} />
         </div>
         <div className={styles.info}>
-          <h6 className={styles.name}>{`${props.nameOriginal} / ${props.nameRu}`}</h6>
+          <h6
+            className={styles.name}
+          >{`${props.nameOriginal} / ${props.nameRu}`}</h6>
           <h6 className={styles.year}>{props.year}</h6>
         </div>
       </article>
