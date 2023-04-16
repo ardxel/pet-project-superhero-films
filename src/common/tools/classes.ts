e7b4e6783b2326ba7f6a4d1c0fd179b3d45180a9
Vasily Bobnev <ardxel@gmail.com>
2023-04-16 18:44:40 +0300

    change classes function.

--- src/common/tools/classes.ts
+++ src/common/tools/classes.ts
@@ -1,3 +1,3 @@
-export default function classes(...className: string[]) {
-  return Array.prototype.join.call(className, ' ')
+export default function classes(...classnames: Array<string | undefined>) {
+  return classnames.reduce((prev, cur) => cur ? `${prev} ${cur}` : prev, '');
 }
