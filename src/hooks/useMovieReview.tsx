3df4ade839e878c220bbd31869ccfeec64189bcd
Vasily Bobnev <ardxel@gmail.com>
2023-04-16 18:39:09 +0300

    wrapped states in usememo and remove useEffect

--- src/hooks/useMovieReview.tsx
+++ src/hooks/useMovieReview.tsx
@@ -1,26 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useMemo } from 'react';
 import { useAppSelector } from '@hooks/useAppSelector';
 
 const useMovieReview = (id: number) => {
   const { favorites, watchlist, ratings } = useAppSelector(
     (state) => state.user
   );
-  const [isFavorite, setIsFavorite] = useState<boolean>(false);
-  const [isInWatchlist, setIsInWatchlist] = useState<boolean>(false);
-  const [isInRatings, setIsInRatings] = useState<boolean>(false);
 
-  useEffect(() => {
-    const isIncludeInFavorite = favorites.includes(id);
-    isIncludeInFavorite ? setIsFavorite(true) : setIsFavorite(false);
+  const isFavorite = useMemo(() => {
+    return favorites.includes(id);
+  },[favorites]);
 
-    const isIncludeInWatchlist = watchlist.includes(id);
-    isIncludeInWatchlist ? setIsInWatchlist(true) : setIsInWatchlist(false);
+  const isInWatchlist = useMemo(() => {
+    return watchlist.includes(id);
+  }, [watchlist]);
 
-    const isIncludeInRatings = ratings.find((item) => item.id === id);
-    isIncludeInRatings ? setIsInRatings(true) : setIsInRatings(false);
-  }, [favorites, watchlist, ratings]);
+  const isInRatings = useMemo(() => {
+    return ratings.find((item) => item.id === id)
+  }, [ratings])
 
   return { isFavorite, isInRatings, isInWatchlist };
+
 };
 
 export default useMovieReview;
